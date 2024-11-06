573852ba6a79e1ed2703bbf9de97a7c5dffb97a0
Harsh Bhardwaj <bhardwajharshmait23@gmail.com>
2024-11-06 23:37:35 +0530

    Create OOPs.cpp

---
+++ 000- OOps/OOPs.cpp
@@ -0,0 +1,39 @@
+// THE SYLLABUS
+
+/*UNIT – I
+Object Oriented Programming Paradigm, Basic Concepts of Object Oriented Programming, Benefits of Object
+Oriented Programming, Object Oriented Languages, Applications of Object Oriented Programming, C++
+Programming Language, Tokens, Keywords, Identifiers and Constants, Data Types, Type Compatibility,
+Variables, Operators in C++, Implicit Type Conversions, Operator Precedence, The Main Function, Function
+Prototyping, Call by Reference, Return by Reference, Inline Functions, Function Overloading, Friend Functions,
+default parameter value.*/
+
+/*UNIT – II
+Specifying a class, Member Functions, Encapsulation, information hiding, abstract data types, objects &
+classes, Static Member Functions, Arrays of Objects, Constructors & Destructors, Parameterized Constructors,
+Copy Constructors, Dynamic Constructors, Destructors, identity and behaviour of an object, C++ garbage
+collection, dynamic memory allocation, Explicit Type Conversions, Operator Overloading.*/
+
+/*UNIT- III
+Inheritance, inheritance methods, Class hierarchy, derivation – public, private & protected, aggregation,
+Inheritance Constructors, composition vs. classification hierarchies, Containership, Initialization List,
+Polymorphism, categorization of polymorphic techniques, polymorphism by parameter, parametric
+polymorphism, generic function – template function, function overriding, run time polymorphism, virtual
+functions*/
+
+/*UNIT – IV
+Standard C++ classes, using multiple inheritance, persistant objects, streams and files, namespaces, exception
+handling, generic classes, standard template library: Library organization and containers, standard containers,
+algorithm and Function objects, iterators and allocators, strings, streams, manipulators, user defined
+manipulators, vectors. */
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+}
