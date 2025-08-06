3142cae6c6e0e617e74d479dd8305815aaa3c4c3
Harsh Bhardwaj <bhardwajharshmait23@gmail.com>
2025-08-06 11:02:38 +0530

    "selection sort"

---
+++ 09-SortingAlgo/selectionSort.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int min;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            swap(nums[i], nums[min]);
+        }
+        return nums;
+    }
+};
